18cca6d7dbd45c7019491afef0409286e0062281
dinesh khadka <dnshka@gmail.com>
2016-01-26 13:27:05 +0545

    Initial tests and fixes

--- js/main.js
+++ js/main.js
@@ -4,21 +4,29 @@ jQuery(function($){
 	var App = {
 
 		init: function() {
-			//pass
+			var $englishButton = $('#english-convert-button');
+			$englishButton.on('click', 
+				function(){
+					var fromDate = getEnglishDate()
+					var crunch = engtonep.DateConversion(fromDate.eDay, fromDate.eMonth, fromDate.eYear);
+					console.log(fromDate.eYear)
+					console.log(crunch.getYear())
+			})
 		}
 	}
 
-	var date = {
 
-		toNepali: function() {
-			//pass
+	var getEnglishDate = function() {
 
-		}
-
-		toEnglish: function(){
-			//pass
+		return {
+			eYear: parseInt($('#english-conversion-form > #english-date-selector option:selected').val()),
+			eMonth: parseInt($('#english-conversion-form > #english-month-selector option:selected').val()),
+			eDay: parseInt($('#english-conversion-form > #english-day-selector option:selected').val())
 
 		}
 	}
 	App.init()
+
+	
+	//console.log(ajako.toEnglish(08, 10, 2072))
 });
