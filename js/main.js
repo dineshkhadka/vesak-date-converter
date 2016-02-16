39ab7c78bfe7fa6357f1cceca39b7663bbf64b57
dinesh khadka <dnshka@gmail.com>
2016-02-16 12:35:00 +0545

    First working version of vesak using milligram css framework

--- js/main.js
+++ js/main.js
@@ -1,32 +1,57 @@
+jQuery(function($) {
+    var englishMonth = ['January','February','March','April','May','June','July','August','september','October','November','December'];
+    var nepaliMonth = ['Baisakh', 'Jestha', 'Ashar', 'Shrawan', 'Bhadra', 'Ashoj', 'Kartik', 'Mangsir', 'Poush', 'Magh', 'Falgun', 'Chaitra'];
+    var App = {
 
-jQuery(function($){
+        init: function() {
+            const $englishButton = $('#english-convert-button');
+            
+            $('#converted-date').html('s')
+            $englishButton.on('click',
+                function() {
+                    var fromDate = getEnglishDate()
+                    var crunch = engtonep.DateConversion(fromDate.eDay, fromDate.eMonth, fromDate.eYear);
+                    //console.log(fromDate.eYear)
+                    //crunch.getYear() + ", " + nepaliMonth[parseInt(crunch.getMonth())] + ", " + crunch.getDay()
+                    $('#converted-date')
+                        .empty()
+                        .append('Your converted date is: ')
+                        .append(
+                                crunch.getYear() +', '+ addSuffix(crunch.getDate())+' of '+ nepaliMonth[parseInt(crunch.getMonth())]
+                            );
+                    //console.log(crunch.getYear())
+                });
 
-	var App = {
-
-		init: function() {
-			var $englishButton = $('#english-convert-button');
-			$englishButton.on('click', 
-				function(){
-					var fromDate = getEnglishDate()
-					var crunch = engtonep.DateConversion(fromDate.eDay, fromDate.eMonth, fromDate.eYear);
-					console.log(fromDate.eYear)
-					console.log(crunch.getYear())
-			})
-		}
-	}
 
 
-	var getEnglishDate = function() {
 
-		return {
-			eYear: parseInt($('#english-conversion-form > #english-date-selector option:selected').val()),
-			eMonth: parseInt($('#english-conversion-form > #english-month-selector option:selected').val()),
-			eDay: parseInt($('#english-conversion-form > #english-day-selector option:selected').val())
+        }
+    }
 
-		}
-	}
-	App.init()
+    var addSuffix = function (theDate){
+        var stamp;
+        if ((theDate >= 11) && (theDate <= 13)) {
+            stamp = 'th';
 
-	
-	//console.log(ajako.toEnglish(08, 10, 2072))
+        } else {
+            var endsWith = theDate % 10,
+                stamp = ((theDate % 100 / 10) === 1) ? 'th' : (endsWith === 1) ? 'st' : (endsWith === 2) ? 'nd' : (endsWith === 3) ? 'rd' : 'th';
+
+        }
+        return theDate + stamp
+    }
+    var getEnglishDate = function() {
+
+        return {
+            eYear: parseInt($('#english-conversion-form > #english-date-selector option:selected').val()),
+            eMonth: parseInt($('#english-conversion-form > #english-month-selector option:selected').val()),
+            eDay: parseInt($('#english-conversion-form > #english-day-selector option:selected').val())
+
+        }
+    }
+    
+    App.init()
+  
+
+    //console.log(ajako.toEnglish(08, 10, 2072))
 });
