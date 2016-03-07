2f8c12ea87d01a15051f98de7bdef50e1f78d592
dinesh khadka <dnshka@gmail.com>
2016-03-07 09:55:45 +0545

    Vesak can now has functions date switchers and can convert dates easily

--- js/main.js
+++ js/main.js
@@ -1,57 +1,141 @@
 jQuery(function($) {
-    var englishMonth = ['January','February','March','April','May','June','July','August','september','October','November','December'];
+    var englishMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'september', 'October', 'November', 'December'];
     var nepaliMonth = ['Baisakh', 'Jestha', 'Ashar', 'Shrawan', 'Bhadra', 'Ashoj', 'Kartik', 'Mangsir', 'Poush', 'Magh', 'Falgun', 'Chaitra'];
-    var App = {
+    var Vesak = {
 
         init: function() {
-            const $englishButton = $('#english-convert-button');
+
+            var $convertButton = $('#convert-button');;
+            var $englishSwitch = $('#english-switch');
+            var $nepaliSwitch = $('#nepali-switch');
+
+            var $englishForm = $('#english-conversion-form');
+            var $nepaliForm = $('#nepali-conversion-form');
+
+            var $switchers = $("#switch-dates input[type='radio']:checked");
+            var $convertedDate = $('#converted-date');
+
+
+
+             
             
+
+            if (Vesak.getCalenderType == "nepali"){
+                $nepaliForm.removeClass('hidden')
+            }
+            else{
+                $englishForm.removeClass('hidden')
+            }
+
+            $nepaliSwitch.on('click', function() {
+                $(this).attr("checked","");
+                $englishSwitch.removeAttr("checked");
+               
+
+            });
+            $englishSwitch.on('click', function() {
+                $(this).attr("checked","");
+                $nepaliSwitch.removeAttr("checked")
+
+            });
+
+            $convertButton.on('click', function(){
+                Vesak.convertDate(Vesak.getCalenderType())
+            });
+
+            $('#switch-dates').on('change', function(){
+                if ( Vesak.getCalenderType() == 'nepali'){
+                    $nepaliForm.removeClass('hidden')
+                    $englishForm.addClass('hidden')
+                    $convertButton.html('Convert To English')
+                } 
+                else {
+                    $englishForm.removeClass('hidden')
+                    $nepaliForm.addClass('hidden')
+                    $convertButton.html('Convert To Nepali')
+                }
+                $('#converted-date').addClass('invisible')
+
+
+            });
+             
+
+
+        
+
             
-            $englishButton.on('click',
-                function() {
-                    var fromDate = getEnglishDate()
-                    var crunch = engtonep.DateConversion(fromDate.eDay, fromDate.eMonth, fromDate.eYear);
-                    //console.log(fromDate.eYear)
-                    //crunch.getYear() + ", " + nepaliMonth[parseInt(crunch.getMonth())] + ", " + crunch.getDay()
-                    $('#converted-date')
-                        .empty()
-                        .append('Your converted date is: ')
-                        .append(
-                                crunch.getYear() +', '+ addSuffix(crunch.getDate())+' of '+ nepaliMonth[parseInt(crunch.getMonth())]
-                            );
-                    //console.log(crunch.getYear())
-                });
 
 
 
 
+        },
+
+        convertDate: function(ctype) {
+                    
+            var crunch;
+            var fromDate = Vesak.getVesakDate(ctype)
+
+            if (ctype == "nepali"){
+                crunch = neptoeng.DateConversion(fromDate.eDay, fromDate.eMonth, fromDate.eYear);
+            }
+            else if (ctype == "english"){
+                crunch = engtonep.DateConversion(fromDate.eDay, fromDate.eMonth, fromDate.eYear);
+            }
+
+            
+            const template = `${crunch.getYear()}, ${Vesak.addSuffix(crunch.getDate())} of ${nepaliMonth[parseInt(crunch.getMonth()) - 1]}`
+            const templateDMY = ` (${crunch.getYear()}/${crunch.getMonth()}/${crunch.getDate()})`
+            $('#converted-date')
+                .removeClass('invisible')
+                .empty()
+                .append('Your converted date is: ')
+                .append(template)
+                .append(templateDMY)
+                
+
+        },
+
+        getCalenderType: function(){
+            return $("#switch-dates input[type='radio']:checked").val();
+
+        },
+
+        addSuffix: function(theDate) {
+            var stamp;
+            if ((theDate >= 11) && (theDate <= 13)) {
+                stamp = 'th';
+
+            } else {
+                var endsWith = theDate % 10,
+                    stamp = ((theDate % 100 / 10) === 1) ? 'th' : (endsWith === 1) ? 'st' : (endsWith === 2) ? 'nd' : (endsWith === 3) ? 'rd' : 'th';
+
+            }
+            return theDate + stamp
+        },
+
+        getVesakDate: function(calenderType) {
+
+            if (calenderType == "english"){
+
+            return {
+                eYear: parseInt($('#english-date-selector option:selected').val()),
+                eMonth: parseInt($('#english-month-selector option:selected').val()),
+                eDay: parseInt($('#english-day-selector option:selected').val())
+
+            }
+        }
+        else if (calenderType == "nepali"){
+            return {
+                eYear: parseInt($('#nepali-date-selector option:selected').val()),
+                eMonth: parseInt($('#nepali-month-selector option:selected').val()),
+                eDay: parseInt($('#nepali-day-selector option:selected').val())
+
+            }
+        }
         }
     }
+    Vesak.init()
 
-    var addSuffix = function (theDate){
-        var stamp;
-        if ((theDate >= 11) && (theDate <= 13)) {
-            stamp = 'th';
-
-        } else {
-            var endsWith = theDate % 10,
-                stamp = ((theDate % 100 / 10) === 1) ? 'th' : (endsWith === 1) ? 'st' : (endsWith === 2) ? 'nd' : (endsWith === 3) ? 'rd' : 'th';
-
-        }
-        return theDate + stamp
-    }
-    var getEnglishDate = function() {
-
-        return {
-            eYear: parseInt($('#english-conversion-form > #english-date-selector option:selected').val()),
-            eMonth: parseInt($('#english-conversion-form > #english-month-selector option:selected').val()),
-            eDay: parseInt($('#english-conversion-form > #english-day-selector option:selected').val())
-
-        }
-    }
-    
-    App.init()
-  
 
     //console.log(ajako.toEnglish(08, 10, 2072))
 });
